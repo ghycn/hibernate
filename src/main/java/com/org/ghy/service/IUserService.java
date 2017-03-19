8ef410ecb58687f172ae731d3e63c606af9c38b3
ghycn <349111145@qq.com>
2017-03-20 00:03:06 +0800

    commit

---
+++ src/main/java/com/org/ghy/service/IUserService.java
@@ -0,0 +1,7 @@
+package com.org.ghy.service;
+
+/**
+ * Created by Administrator on 2017/3/19.
+ */
+public interface IUserService {
+}
---
+++ src/main/java/com/org/ghy/service/impl/UserServiceImpl.java
@@ -0,0 +1,9 @@
+package com.org.ghy.service.impl;
+
+import com.org.ghy.service.IUserService;
+
+/**
+ * Created by Administrator on 2017/3/20.
+ */
+public class UserServiceImpl implements IUserService{
+}
